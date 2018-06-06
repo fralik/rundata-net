ed3532763f5f8a341c464df911029a5f21887f98
Vadim Frolov <fralik@gmail.com>
2018-06-06 10:04:49 +0200

    fix: Fix previous commit, add forgotten file

---
+++ rundatanet/runes/sitemaps.py
@@ -0,0 +1,32 @@
+from django.contrib import sitemaps
+from django.urls import reverse
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['runes:references']
+
+    def location(self, item):
+        return reverse(item)
+
+class MainPageSitemap(sitemaps.Sitemap):
+    priority = 0.9
+    changefreq = 'daily'
+
+    def items(self):
+        return ['runes:index']
+
+    def location(self, item):
+        return reverse(item)
+
+class AoutPageSitemap(sitemaps.Sitemap):
+    priority = 1.0
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['runes:about']
+
+    def location(self, item):
+        return reverse(item)
