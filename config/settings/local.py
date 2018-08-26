2bf3599c72a733266a2da597f2ad59f0e19dea79
Vadim Frolov <fralik@gmail.com>
2018-08-27 01:04:38 +0200

    Add docker for local development
    Add necessary files for starting Docker in development mode. Run it with ```sudo docker-compose -f local.yml build``` and ```sudo docker-compose -f local.yml up```.

--- config/settings/local.py
+++ config/settings/local.py
@@ -55,14 +55,17 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG  # noqa F405
 #     ],
 #     'SHOW_TEMPLATE_CONTEXT': True,
 # }
-# # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-# INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
-
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
+if env('USE_DOCKER') == 'yes':
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
 
 # django-extensions
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
-# INSTALLED_APPS += ['django_extensions']  # noqa F405
+INSTALLED_APPS += ['django_extensions']  # noqa F405
 
 # Your stuff...
 # ------------------------------------------------------------------------------
