04801506ad44a818a8dd4b8613284bd4e6628768
Vadim Frolov <fralik@gmail.com>
2018-10-02 21:28:19 +0200

    Full csv export; prevent jump after resetting search results
    * CSV export now supports crosses, coordinates and escapes quotes. It shall be possible to open csv files in Excel without any problems.
    * Prevent main display from continuous jump after user resets the search results. The jump could have been observed by searching for metal inscriptions from Gotland. When search results were ready, one could select some signature from the middle of the list, then click Reset search button. After that the main display would have jumped for a while.

--- rundatanet/static/runes/csv.worker.js
+++ rundatanet/static/runes/csv.worker.js
@@ -22,8 +22,49 @@ onmessage = function(e) {
         colData = row['signature_text_raw'];
       }
 
+      if (columnName == 'crosses') {
+        colData = [];
+        let crossesObj = row[columnName];
+        for (var cId = 0; cId < crossesObj.length; cId++) {
+          var crossObj = crossesObj[cId];
+          var oneCross = [];
+
+          if (crossObj[0].length > 0) {
+            // this cross has something in group 0, thus it must not
+            // contain anything in other groups
+            colData.push(crossObj[0][0].name);
+            continue;
+          }
+
+          for (var gId = 1; gId < crossObj.length; gId++) {
+            var groupItems = crossObj[gId];
+            if (groupItems.length == 0) {
+              oneCross.push('');
+              continue;
+            }
+
+            var oneGroup = [];
+            for (var fId = 0; fId < groupItems.length; fId++) {
+              var crossForm = groupItems[fId];
+              if (crossForm.isCertain) {
+                oneGroup.push(crossForm.name);
+              } else {
+                oneGroup.push(`${crossForm.name} ?`);
+              }
+            }
+            oneCross.push(oneGroup.join(',')); // join individual cross forms with ,
+          }
+          colData.push(oneCross.join(';')); // join groups with ;
+        }
+        colData = colData.join(' & '); // join crosses with &
+      }
+
       if (colData.constructor === Array) {
         colData = colData.join(';');
+      }
+
+      if (typeof colData === 'string' || colData instanceof String) {
+        colData = colData.replace(/"/g, '""');
       }
 
       rowData.push('"' + colData + '"');
