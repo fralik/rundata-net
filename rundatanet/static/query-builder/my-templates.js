0100880212b0b45dcee8151c069b81cde4cec032
Vadim Frolov <fralik@gmail.com>
2024-09-24 11:57:08 +0200

    Add search in signatures by 'in' operator; update dependencies
    * It is now possible to search inscriptions by using 'in' operator and selectize plugin. This allows straightforward searches when one has a list of signatures to search for.
    * Update versions of some of the dependencies.
    * Updated code that imports result files. Ensured that all files that are referenced in our documentation are possible to import.
    
    Fixes #10

--- rundatanet/static/query-builder/my-templates.js
+++ rundatanet/static/query-builder/my-templates.js
@@ -1,41 +1,44 @@
 /* QueryBuilder templates without wrapping add/delete buttons into Bootstrap's button group
+   Changes: remove btn-group for add_rule/group, make buttons btn-xs
  * Use it during builder initialization as:
     {
       templates: {
-        group: gBuilderCustomGroup,
+        group: gQueryBuilderCustomGroup,
       }
   }
  */
-var gBuilderCustomGroup = '\
-<div id="{{= it.group_id }}" class="rules-group-container"> \
-  <div class="rules-group-header"> \
-    <div class="pull-right group-actions"> \
-      <button type="button" class="btn btn-xs btn-info" data-add="rule"> \
-        <i class="{{= it.icons.add_rule }}"></i> {{= it.translate("add_rule") }} \
-      </button> \
-      {{? it.settings.allow_groups===-1 || it.settings.allow_groups>=it.level }} \
-        <button type="button" class="btn btn-xs btn-info" data-add="group"> \
-          <i class="{{= it.icons.add_group }}"></i> {{= it.translate("add_group") }} \
-        </button> \
-      {{?}} \
-      {{? it.level>1 }} \
-        <button type="button" class="btn btn-xs btn-danger" data-delete="group"> \
-          <i class="{{= it.icons.remove_group }}"></i> {{= it.translate("delete_group") }} \
-        </button> \
-      {{?}} \
-    </div> \
-    <div class="btn-group group-conditions"> \
-      {{~ it.conditions: condition }} \
-        <label class="btn btn-xs btn-primary"> \
-          <input type="radio" name="{{= it.group_id }}_cond" value="{{= condition }}"> {{= it.translate("conditions", condition) }} \
-        </label> \
-      {{~}} \
-    </div> \
-    {{? it.settings.display_errors }} \
-      <div class="error-container"><i class="{{= it.icons.error }}"></i></div> \
-    {{?}} \
-  </div> \
-  <div class=rules-group-body> \
-    <div class=rules-list></div> \
-  </div> \
-</div>';
+const gQueryBuilderCustomGroup = ({ group_id, level, conditions, icons, settings, translate, builder }) => {
+  return `
+    <div id="${group_id}" class="rules-group-container">
+      <div class="rules-group-header">
+        <div class="pull-right group-actions">
+          <button type="button" class="btn btn-xs btn-info" data-add="rule">
+            <i class="${icons.add_rule}"></i> ${translate("add_rule")}
+          </button>
+          ${settings.allow_groups === -1 || settings.allow_groups >= level ? `
+            <button type="button" class="btn btn-xs btn-info" data-add="group">
+              <i class="${icons.add_group}"></i> ${translate("add_group")}
+            </button>
+          ` : ''}
+          ${level > 1 ? `
+            <button type="button" class="btn btn-xs btn-danger" data-delete="group">
+              <i class="${icons.remove_group}"></i> ${translate("delete_group")}
+            </button>
+          ` : ''}
+        </div>
+        <div class="btn-group group-conditions">
+          ${conditions.map(condition => `
+            <label class="btn btn-xs btn-primary">
+              <input type="radio" name="${group_id}_cond" value="${condition}"> ${translate("conditions", condition)}
+            </label>
+          `).join('\n')}
+        </div>
+        ${settings.display_errors ? `
+          <div class="error-container"><i class="${icons.error}"></i></div>
+        ` : ''}
+      </div>
+      <div class=rules-group-body>
+        <div class=rules-list></div>
+      </div>
+    </div>`;
+};
