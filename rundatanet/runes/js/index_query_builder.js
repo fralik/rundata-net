4df1e606af33f7a186a9dcf74f51e09d1adf059e
Vadim Frolov <fralik@gmail.com>
2025-03-08 13:51:22 +0100

    Add ci/cd pipeline for PRs
    - Also add forgotten file

---
+++ rundatanet/runes/js/index_query_builder.js
@@ -0,0 +1,514 @@
+/*
+This file contains code to work with jquery query builder. The query builder must be
+included in your code prior to using this file.
+*/
+
+const queryBuilderPlugins = {
+  'bt-tooltip-errors': null,
+  'sortable': null,
+  'not-group': null,
+  'case-rule': null,
+};
+
+const optGroups = {
+  "gr_signature": {
+    "en": "Inscription ID",
+    "sv": "Signatura",
+  },
+  "gr_texts": "Texts",
+  "other": "---",
+};
+
+// QueryBuilder plugin for case-(in)sensitive search
+$.fn.queryBuilder.define('case-rule', function(options) {
+  let self = this;
+
+  // Bind events
+  this.on('afterInit', function() {
+    self.$el.on('click.queryBuilder', '[data-case=rule]', function () {
+      let $rule = $(this).closest($.fn.queryBuilder.constructor.selectors.rule_container);
+      let rule = self.getModel($rule);
+      rule.ignoreCase = !rule.ignoreCase;
+      console.log(`afterInit: ${rule.id}, ignoreCase: ${rule.ignoreCase}`);
+      // print rule configuration
+      console.log(rule);
+    });
+
+    self.model.on('update', function(e, node, field) {
+      if (node instanceof $.fn.queryBuilder.constructor.Rule && field === 'ignoreCase') {
+        console.log(`update: ${node.id}, ignoreCase: ${node.ignoreCase}`);
+        self.updateRuleCaseIgnore(node);
+      }
+    });
+  });
+
+  // Init case-sensitivity property
+  this.on('afterAddRule', function(e, rule) {
+    rule.__.ignoreCase = false;
+  });
+
+  this.on('afterCreateRuleInput.filter', function(e, rule) {
+    // Show plugin's button only for text fields
+    if (!rule.filter || typeof rule.filter.input !== 'string') {
+      rule.$el.find(cssSelectorPluginCaseRule).hide();
+      return;
+    }
+
+    if (rule.filter.input.indexOf('text') === -1) {
+      rule.$el.find(cssSelectorPluginCaseRule).hide();
+    } else {
+      rule.$el.find(cssSelectorPluginCaseRule).show();
+    }
+  });
+
+  // Modify templates
+  if (!options.disable_template) {
+    this.on('getRuleTemplate.filter', function(h) {
+      var $h = $($.parseHTML(h.value));
+      $h.find($.fn.queryBuilder.constructor.selectors.rule_actions).prepend(
+          '\n<button type="button" class="btn btn-xs btn-default" active data-case="rule">' +
+          '<i class="' + options.icon_checked + '"></i> ' +
+          '<span class="case-rule-text">' + self.translate('Match case') + '</span>' +
+          '</button>'
+      );
+      h.value = $h.prop('outerHTML');
+    });
+  }
+
+  // Export "case-rule" to JSON
+  this.on('ruleToJson.filter', function(e, rule) {
+    e.value.ignoreCase = rule.ignoreCase;
+  });
+
+  // Read "case-rule" from JSON
+  this.on('jsonToRule.filter', function(e, json) {
+    e.value.ignoreCase = !!json.ignoreCase;
+  });
+
+  // Export case selector to SQL
+  this.on('ruleToSQL.filter', function(e, rule, value, sqlFn) {
+    console.log(`ruleToSQL.filter: ${rule.id}, ignoreCase: ${rule.ignoreCase}`);
+    if (rule.ignoreCase) {
+      e.value = 'NOCASE ( ' + e.value + ' )';
+    }
+  });
+}, {
+  icon_checked: 'bi bi-check2-square',
+  disable_template: false
+});
+
+$.fn.queryBuilder.constructor.utils.defineModelProperties($.fn.queryBuilder.constructor.Rule, ['ignoreCase']);
+
+const cssSelectorPluginCaseRule = $.fn.queryBuilder.constructor.selectors.rule_actions + ' [data-case=rule]';
+
+$.fn.queryBuilder.extend({
+  /**
+   * Performs actions when a rule's case selector changes
+   * @param {Rule} rule
+   * @fires module:plugins.CaseSelector.updateRuleCaseIgnore
+   * @private
+   */
+  updateRuleCaseIgnore: function(rule) {
+      console.log(`updateRuleCaseIgnore: ${rule.id}, ignoreCase: ${rule.ignoreCase}`);
+      rule.$el.find(cssSelectorPluginCaseRule + "> .case-rule-text")
+          .text(this.translate(rule.ignoreCase ? 'Ignore case' : 'Match case'));
+
+      /**
+       * After the rule's case selector has been modified
+       * @event afterUpdateRuleCaseSelector
+       * @memberof module:plugins.CaseSelector
+       * @param {Rule} rule
+       */
+      this.trigger('afterUpdateRuleCaseSelector', rule);
+
+      this.trigger('rulesChanged');
+  }
+});
+
+
+ // Function to sort filters within each optgroup
+ function sortFilters(filters) {
+  var groupedFilters = {};
+
+  // Group filters by optgroup
+  filters.forEach(function(filter) {
+      if (!groupedFilters[filter.optgroup]) {
+          groupedFilters[filter.optgroup] = [];
+      }
+      groupedFilters[filter.optgroup].push(filter);
+  });
+
+  // Sort filters within each optgroup
+  for (var group in groupedFilters) {
+      groupedFilters[group].sort(function(a, b) {
+          return a.label.localeCompare(b.label);
+      });
+  }
+
+  // Flatten the sorted filters back into a single array
+  var sortedFilters = [];
+  for (var group in groupedFilters) {
+      sortedFilters = sortedFilters.concat(groupedFilters[group]);
+  }
+
+  return sortedFilters;
+}
+
+
+function getValuesFromAllData(term, suggest, fieldName, dbMap, isTomSelect = false) {
+  // Get all unique values from dbMap for the specified fieldName
+  const allValues = [];
+  const uniqueTracker = new Set();
+  let nextArtificialId = 20000; // Starting ID for aliases
+
+  Array.from(dbMap.values()).forEach(item => {
+    const visited = uniqueTracker.has(item[fieldName]);
+    if (item[fieldName] && item[fieldName] !== '' && !visited) {
+      uniqueTracker.add(item[fieldName]);
+      allValues.push({
+        text: item[fieldName],
+        id: item[fieldName],
+        score: item.id || 0
+      });
+      if (item.aliases) {
+        const aliases = item.aliases.split('|').map(a => a.trim()).filter(a => a);
+        
+        // Add each alias with an artificial ID
+        aliases.forEach(alias => {
+          if (!uniqueTracker.has(alias)) {
+            uniqueTracker.add(alias);
+            allValues.push({
+              text: alias,
+              id: alias,
+              score: nextArtificialId++,
+            });
+          }
+        });
+      }
+    }
+  });
+  
+  allValues.sort((a, b) => a.score - b.score);
+  
+  // Comparison without diacritics:
+  if (term !== '') {
+    // Normalize strings to remove diacritics
+    const normalizedTerm = term.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+    allValues = allValues.filter(item => {
+      const normalizedText = item.text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+      return normalizedText.includes(normalizedTerm);
+    });
+  }
+  
+  if (isTomSelect) {
+    suggest(allValues);
+    return;
+  }
+
+  const values = allValues.map(item => item.text);
+  suggest(values);
+}
+
+
+/**
+ * Creates an autocomplete configuration object for QueryBuilder.
+ * 
+ * @param {string} ruleId - The unique identifier for the rule.
+ * @param {Map} dbMap - A Map containing the database values for autocomplete.
+ * @param {Function} humanNameGetter - Function that returns a human-readable name for the given rule ID.
+ * @param {Object} [opt={}] - Optional configuration parameters.
+ * @param {string} [opt.fieldId] - The field ID to use (defaults to ruleId if not provided).
+ * @param {string[]} [opt.operators] - Array of operators to use with this field.
+ * @param {string} [opt.type='string'] - The data type for the field.
+ * @param {number} [opt.size=100] - The display size of the field.
+ * @param {string} [opt.optgroup='other'] - The option group to which this field belongs.
+ * @returns {Object} Configuration object for QueryBuilder autocomplete field.
+ * @throws {Error} If required parameters are missing or invalid.
+ */
+function prepareAutoComplete(ruleId, dbMap, humanNameGetter, opt = {}) {
+  // Check required arguments
+  if (ruleId === undefined) {
+    throw new Error("prepareAutoComplete: 'ruleId' parameter is required");
+  }
+  if (!dbMap || !(dbMap instanceof Map)) {
+    throw new Error("prepareAutoComplete: 'dbMap' parameter is required and must be a Map");
+  }
+  if (!humanNameGetter || typeof humanNameGetter !== 'function') {
+    throw new Error("prepareAutoComplete: 'humanNameGetter' parameter is required and must be a function");
+  }
+  
+  const fieldId = opt.fieldId || ruleId;
+  const operators = opt.operators || ["my_contains", "my_not_contains",
+        'my_equal', 'my_not_equal', 'my_begins_with',
+        "my_not_begins_with", "my_ends_with",
+        "my_not_ends_with", "is_empty", 'is_not_empty'];
+  const type = opt.type || 'string';
+  const size = opt.size || 100;
+  const optgroup = opt.optgroup || "other";
+
+  return {
+    id: ruleId,
+    field: fieldId,
+    optgroup: optgroup,
+    label: humanNameGetter(ruleId),
+    type: type,
+    plugin: 'autoComplete',
+    plugin_config: {
+      minChars: 0,
+      delay: 100,
+      source: function (term, suggest) {
+        getValuesFromAllData(term, suggest, fieldId, dbMap);
+      },
+      menuClass: ' clusterize-content ',
+      attachToParent: true,
+    },
+    size: size,
+    // operators: operators,
+  }
+}
+/**
+ * Adjusts the input element in a query rule by applying either TomSelect or AutoComplete plugin
+ * based on the rule's operator type.
+ * 
+ * @param {Object} rule - The query rule object containing the element and operator information
+ * @param {Object} [tomSelectConfig={}] - Configuration options for TomSelect plugin
+ * @param {Object} [autoCompleteConfig={}] - Configuration options for AutoComplete plugin
+ * 
+ * @description This function first cleans up any existing TomSelect or AutoComplete plugins
+ * attached to the input element, then initializes the appropriate plugin based on the
+ * operator type. If the operator is 'in' or 'not_in', TomSelect is applied; otherwise,
+ * AutoComplete is used.
+ */
+function adjustTomSelectAndAutoComplete(rule, tomSelectConfig = {}, autoCompleteConfig = {}) {
+  var $input = rule.$el.find('.rule-value-container input');
+  const operator = rule.operator.type;    
+
+  // Clean up existing plugins
+  if ($input.data('tomSelect') !== undefined) {
+    $input.tomSelect('destroy');
+  }
+  if ($input.hasClass('autocomplete')) {
+    $input.autoComplete('destroy');
+  }
+  
+  // Initialize the appropriate plugin based on operator
+  if (operator === 'in' || operator === 'not_in') {
+    $input.tomSelect(tomSelectConfig);
+  } else {
+    $input.autoComplete(autoCompleteConfig);
+  }
+}
+
+
+export function initQueryBuilder(containerId, dbMap, getHumanName) {
+  const queryBuilder = $(`#${containerId}`);
+
+
+  const qbOperators = $.fn.queryBuilder.constructor.DEFAULTS.operators.concat([
+    // Add to default operators
+    { type: 'in_separated_list', nb_inputs: 1, multiple: false, apply_to: ['string'] },
+  ]);
+  const qbLang = {
+    operators: {
+      'in_separated_list': "is in |-separated list",
+    }
+  };
+  const qbSqlOperators = {
+    'in_separated_list': { 'op': 'IN', mod: '{0}' },
+  };
+
+  const signature_text_autocomplete_cfg = {
+    minChars: 0,
+    delay: 100,
+    source: function(term, suggest) {
+      getValuesFromAllData(term, suggest, 'signature_text', dbMap);
+    },
+    menuClass: 'clusterize-content',
+    attachToParent: true,
+  };
+  
+  const signature_text_tomselect_cfg = {
+    plugins: ['remove_button'],
+    // options: getValuesFromAllData('', undefined, 'signature_text', dbMap),
+    load: function(query, callback) {
+      let self = this;
+      getValuesFromAllData(query, callback, 'signature_text', dbMap, true);
+      // prevent further loading
+      self.settings.load = null;
+    },
+    // invoke data loading at once
+    preload: true,
+    valueField: 'id',
+    hideSelected: true,
+    delimiter: '|',
+  };  
+  
+  let queryBuilderFilters = [
+    // the order of items here defines the order of optgroups, place filters of at least each group in the order you like
+    // filters within a group will be sorted alphabetically
+    // {
+    //   id: 'signature_text',
+    //   optgroup: 'gr_signature',
+    //   field: 'signature_text',
+    //   label: 'Signature',
+    //   type: 'string',
+    //   input: 'select',
+    //   plugin: 'select2',
+    //   plugin_config: {
+    //     multiple: true,
+    //     data: locationSignatures,
+    //   },
+    //   //values: locationSignatures,
+    // },
+    // {
+    //   id: 'signature_text',
+    //   optgroup: 'gr_signature',
+    //   field: 'signature_text',
+    //   label: getHumanName('signature_text'),
+    //   type: 'string',
+    //   input: 'select',
+    //   multiple: true,
+    //   operators: [
+    //     'in', 'in_separated_list', 'begins_with', 'not_begins_with',
+    //     'ends_with', 'not_ends_with', 'contains', 'not_contains',
+    //   ],
+    //   plugin: 'tomSelect',
+    //   plugin_config: {
+    //     plugins: ['remove_button'],
+    //     options: allSignatures,
+    //     hideSelected: true,
+    //   },
+    // },
+    {
+      id: 'signature_text',
+      optgroup: 'gr_signature',
+      field: 'signature_text',
+      label: getHumanName('signature_text'),
+      type: 'string',
+      multiple: true,
+      operators: [
+        'in', 'in_separated_list', 'begins_with', 'not_begins_with',
+        'ends_with', 'not_ends_with', 'contains', 'not_contains',
+      ],
+      valueSetter: function (rule, value) {
+        const $input = rule.$el.find('.rule-value-container input');
+        const operator = rule.operator.type;
+        if (operator === 'in' || operator === 'not_in') {
+          $input.tomSelect('setValue', value);
+        } else {
+          // Ensure autoComplete is initialized before using it
+          if ($input[0].autoComplete) {
+            $input[0].autoComplete.setValue(value);
+          } else {
+            $input.val(value);
+          }
+        }
+      },
+      plugin: 'tomSelect',
+      plugin_config: signature_text_tomselect_cfg,
+    },
+    prepareAutoComplete('carver', dbMap, getHumanName),
+    {
+      id: 'signature_country',
+      optgroup: "gr_signature",
+      field: 'signature_text',
+      label: 'Country or Swedish province',
+      type: 'string',
+      input: 'select',
+      multiple: true,
+      operators: ['in'],
+      // valueSetter: function (rule, value) {
+      //   rule.$el.find('.rule-value-container select')[0].selectize.setValue(value);
+      // },
+      plugin: 'tomSelect',
+      plugin_config: {
+        plugins: ['remove_button'],
+        options: [
+          {text: 'Sweden, whole', value: 'all_sweden'},
+          {text: 'Öland (Öl)', value: 'Öl '}, {text: 'Östergötland (Ög)', value: 'Ög '}, {text: 'Södermanland (Sö)', value: 'Sö '},
+          {text: 'Småland (Sm)', value: 'Sm '}, {text: 'Västergötland (Vg)', value: 'Vg '}, {text: 'Uppand (U)', value: 'U '},
+          {text: 'Västmanland (Vs)', value: 'Vs '}, {text: 'Närke (Nä)', value: 'Nä '}, {text: 'Värmland (Vr)', value: 'Vr '},
+          {text: 'Gästrikland (Gs)', value: 'Gs '}, {text: 'Hälsingland (Hs)', value: 'Hs '}, {text: 'Medelpad (M)', value: 'M '},
+          {text: 'Ångermanland (Ån)', value: 'Ån '}, {text: 'Dalarna (D)', value: 'D '}, {text: 'Härjedalen (Hr)', value: 'Hr '},
+          {text: 'Jämtland (J)', value: 'J '}, {text: 'Lappland (Lp)', value: 'Lp '}, {text: 'Dalsland (Ds)', value: 'Ds '},
+          {text: 'Bohuslän (Bo)', value: 'Bo '}, {text: 'Gotland (G)', value: 'G '}, {text: 'Sweden, other (SE)', value: 'SE '},
+          {text: 'Denmark (DR)', value: 'DR '}, {text: 'Norway (N)', value: 'N '}, {text: 'Faroe Islands (FR)', value: 'FR '},
+          {text: 'Greenland (GR)', value: 'GR '}, {text: 'Iceland (IS)', value: 'IS '}, {text: 'Finland (FI)', value: 'FI '},
+          {text: 'Shetland (Sh)', value: 'Sh '}, {text: 'Orkney (Or)', value: 'Or '}, {text: 'Scotland (Sc)', value: 'Sc '},
+          {text: 'England (E)', value: 'E '}, {text: 'Isle of Man (IM)', value: 'IM '}, {text: 'Ireland (IR)', value: 'IR '},
+          {text: 'France (F)', value: 'F '}, {text: 'Netherlands (NL)', value: 'NL '}, {text: 'Germany (DE)', value: 'DE '},
+          {text: 'Poland (PL)', value: 'PL '}, {text: 'Latvia (LV)', value: 'LV '}, {text: 'Russia (RU)', value: 'RU '},
+          {text: 'Ukraine (UA)', value: 'UA '}, {text: 'Byzantium (By)', value: 'By '}, {text: 'Italy (IT)', value: 'IT '},
+          {text: 'Other areas (X)', value: 'X '}
+        ],
+        hideSelected: true,
+        // valueField: 'v',
+        // labelField: 'text',
+        // searchField: 'text',
+        // sortField: 'text',
+        // create: false,
+        // dropdownDirection: 'up',
+        // maxItems: null,
+      },
+    },
+    prepareAutoComplete('normalisation_norse', dbMap, getHumanName, {fieldId: 'normalisation_search_norse', optgroup: "gr_texts"}),
+  ];
+
+  const my_rule_template = ({ rule_id, icons, settings, translate, builder }) => {
+    return `
+  <div id="${rule_id}" class="rule-container d-flex align-items-center w-100">
+    <div class="rule-header">
+    </div>
+    ${settings.display_errors ? `
+      <div class="error-container flex-shrink-0"><i class="${icons.error}"></i></div>
+    ` : ''}
+    <div class="rule-filter-container flex-shrink-0"></div>
+    <div class="rule-operator-container flex-shrink-0"></div>
+    <div class="rule-value-container flex-grow-1 me-2"></div>
+    <div class="rule-footer d-flex align-items-center ms-auto">
+      <div class="btn-group flex-shrink-0 rule-actions">
+        <button type="button" class="btn btn-sm btn-danger" data-delete="rule">
+          <i class="${icons.remove_rule}"></i> ${translate("delete_rule")}
+        </button>
+      </div>
+    </div>
+  </div>`;
+  };
+
+  // sort into groups
+  queryBuilderFilters = $.fn.queryBuilder.constructor.utils.groupSort(queryBuilderFilters, 'optgroup')
+  // sort within groups
+  queryBuilderFilters = sortFilters(queryBuilderFilters);
+
+  // swap two first filters, so that signature is on the first place!
+  const tmp = queryBuilderFilters[0];
+  queryBuilderFilters[0] = queryBuilderFilters[1];
+  queryBuilderFilters[1] = tmp;  
+
+  queryBuilder.queryBuilder({
+    display_empty_filter: false,
+
+    plugins: queryBuilderPlugins,
+    filters: queryBuilderFilters,
+    sort_filters: false,
+    allow_empty: false,
+    optgroups: optGroups,
+
+    operators: qbOperators,
+    lang: qbLang,
+    sqlOperators: qbSqlOperators,    
+
+    templates: {
+      rule: my_rule_template,
+    },
+  });
+
+  // Event handler when rule is created and rule operator is changed
+  $('#builder').on('afterCreateRuleInput.queryBuilder afterUpdateRuleOperator.queryBuilder', function(e, rule) {
+    if (rule.filter.id !== 'signature_text') {
+      return;
+    }
+    adjustTomSelectAndAutoComplete(rule, signature_text_tomselect_cfg, signature_text_autocomplete_cfg);
+  });
+ 
+}
